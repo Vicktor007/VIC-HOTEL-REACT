baee4095c746800aa9ca2de67180586bc15d48cc
Vicktor007 <vicktord007@gmail.com>
2024-10-08 19:27:03 +0100

    navbar

---
+++ src/component/common/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import ApiService from "../../service/ApiService";
+
+
+function Navbar(){
+    
+    const isAuthenticated = ApiService.isAuthenticated();
+    const isAdmin = ApiService.isAdmin();
+    const isUser = ApiService.isUser();
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        const isLogout = window.confirm("Are you sure you want to logout?");
+        if (isLogout) {
+            ApiService.logout();
+            navigate("/home");
+        }
+    }
+
+
+    return(
+        <nav>
+            <div className="">
+                <ui>
+                    <li>
+                       <NavLink to="/home" activeClass="active">
+                        Home
+                        </NavLink> 
+                        <li><NavLink to="/rooms" activeclassname="active">Rooms</NavLink></li>
+                <li><NavLink to="/find-booking" activeclassname="active">Find my Booking</NavLink></li>
+
+                {isUser && <li><NavLink to="/profile" activeclassname="active">Profile</NavLink></li>}
+                {isAdmin && <li><NavLink to="/admin" activeclassname="active">Admin</NavLink></li>}
+
+                {!isAuthenticated &&<li><NavLink to="/login" activeclassname="active">Login</NavLink></li>}
+                {!isAuthenticated &&<li><NavLink to="/register" activeclassname="active">Register</NavLink></li>}
+                {isAuthenticated && <li onClick={handleLogout}>Logout</li>}
+                    </li>
+                </ui>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
