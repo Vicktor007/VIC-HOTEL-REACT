2fe0db584a902560263d52650f7c8188aeac27e7
Vicktor007 <vicktord007@gmail.com>
2024-10-19 08:39:36 +0100

    final touch?

--- src/component/booking&Rooms/BookingDetailsPage.jsx
+++ src/component/booking&Rooms/BookingDetailsPage.jsx
@@ -1,21 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import ApiService from '../../service/ApiService'; // Assuming your service is in a file called ApiService.js
-import { useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import LoaderV2 from '../LoaderV2';
 
 const BookingDetails = () => {
 
+    const location = useLocation()
+
 const {confirmationCode} = useParams();
 const navigate  = useNavigate();
-   
+   const  isAuthenticated = ApiService.isAuthenticated()
     
     const [bookingDetails, setBookingDetails] = useState(null); // State variable for booking details
     const [error, setError] = useState(null); // Track any errors
     const [successMessage, setSuccessMessage] = useState(null);
     const [loading, setLoading] = useState(false);
 
+
+
+
     useEffect (() => {
         const fetchData = async () => {
+
+            
+
 
             try {
                 setLoading(true);
@@ -34,25 +42,30 @@ const navigate  = useNavigate();
     },[confirmationCode])
 
     const cancelBooking = async (bookingId) => {
-        if (!window.confirm('Are you sure you want to cancel this booking?')) {
-            return; // Do nothing if the user cancels
-        }
-
-        try {
-            setLoading(true);
-            const response = await ApiService.cancelBooking(bookingId);
-            if (response.statusCode === 200) {
-                setSuccessMessage("The booking was Successfully cancelled")
-                setLoading(false);
-                setTimeout(() => {
-                    setSuccessMessage('');
-                }, 3000);
-                navigate("/profile")
+        
+        if (!isAuthenticated) {
+            navigate("/login", { replace: true, state: { from: location } });
+        } else {
+            if (!window.confirm('Are you sure you want to cancel this booking?')) {
+                return; // Do nothing if the user cancels
             }
-        } catch (error) {
-            setLoading(false);
-            setError(error.response?.data?.message || error.message);
-            setTimeout(() => setError(''), 5000);
+    
+            try {
+                setLoading(true);
+                const response = await ApiService.cancelBooking(bookingId);
+                if (response.statusCode === 200) {
+                    setSuccessMessage("The booking was Successfully cancelled")
+                    setLoading(false);
+                    setTimeout(() => {
+                        setSuccessMessage('');
+                    }, 3000);
+                    navigate("/profile")
+                }
+            } catch (error) {
+                setLoading(false);
+                setError(error.response?.data?.message || error.message);
+                setTimeout(() => setError(''), 5000);
+            }
         }
     };
 
@@ -94,10 +107,13 @@ const navigate  = useNavigate();
                         <p> Room Type: {bookingDetails.room.roomType}</p>
                         <img src={bookingDetails.room.roomPhotoUrl} alt="" sizes="" srcSet="" />
                     </div>
-                    <button
+
+                            <button
                                     className="profile-cancel-booking"
                                     onClick={() => cancelBooking(bookingDetails.id)}>Cancel Booking
-                                </button>
+                            </button>
+                   
+                    
                 </div>
             )}
         </div>
